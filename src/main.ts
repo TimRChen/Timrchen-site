e17c960e85faa4c911f688983ec722068c8bc3ef
Tim <timrchen@foxmail.com>
2018-09-19 15:19:40 +0800

    construction preparations.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
